57b2ddf575677b3f2edbf9ae94b3e09f44aac154
Isfandiyor777 <139679142+Isfandiyor777@users.noreply.github.com>
2023-08-10 15:39:46 +0300

    Add files via upload

---
+++ Skibidi Bot.py
@@ -0,0 +1,12 @@
+print("                     Skibidi-Bot4                     ")
+print(" ")
+print("Hello")
+text = input()
+if (text == "Hello") and (text == "Hi"):
+    print("How are you?")
+    text = input()
+    if (text == "Good") and (text == "Fine"):
+        print("What are you doing?")
+        text = input()
+        if (text ==  "Playing"):
+            print("BAD")
