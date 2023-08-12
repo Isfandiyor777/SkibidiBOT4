5027ff3a3d104dfa6d1cc10d3f37c2d9693fbbb1
Isfandiyor777 <139679142+Isfandiyor777@users.noreply.github.com>
2023-08-12 23:35:41 +0500

    Update Skibidi Bot4.py
    Homework

--- Skibidi Bot.py
+++ Skibidi Bot.py
@@ -2,11 +2,25 @@ print("                     Skibidi-Bot4                     ")
 print(" ")
 print("Hello")
 text = input()
-if (text == "Hello") and (text == "Hi"):
+if (text == "hello") | (text == "hi"):
     print("How are you?")
     text = input()
-    if (text == "Good") and (text == "Fine"):
+    if (text == "good") | (text == "fine"):
         print("What are you doing?")
         text = input()
-        if (text ==  "Playing"):
+        if (text ==  "playing"):
             print("BAD")
+            text = input()
+            if (text == "that's your bad") | (text == "really?"):
+                print("Don't care") or print("Yeah")
+                text = input()
+                if (text == "What are you doing?"):
+                    print("# IQ 1000 moment")
+                    print("Talking with you")
+    elif (text == "bad"):
+        print("What happened?")
+        text = input()
+        if (text == "I got hurt") | (text == "Im feeling bad"):
+            print("Let's go to the doctor")
+    else: 
+        print("Made with Python 3.11                                  Creator: Inventor Storm")
